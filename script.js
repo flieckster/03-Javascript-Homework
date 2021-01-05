baa163d62d1170ce458764f02b3cc26dfbe29a7d
Brian Flieck <Brianflieck@gmail.com>
2021-01-05 14:55:06 -0500

    updated JS with some variables

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// variables of each requirment in array format
+var number = ["0, 1, 2, 3, 4, 5, 6, 7, 8, 9"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "~", "_", "-"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
